a3d152855b31a2a06c27f7095b8f1160b85da5e5
unclejet <hanjintian@ypzdw.com>
2019-11-20 15:13:57 +0800

    feat(ming ming random): assemble Main finished

---
+++ coding-dojo-huawei/src/main/java/com/uj/study/mingming_random/Main.java
@@ -0,0 +1,17 @@
+package com.uj.study.mingming_random;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        LineReader lineReader = () -> new Scanner(System.in).nextLine();
+        UserInputReader userInputReader = new UserInputReader(lineReader);
+        int size = userInputReader.readArraySize();
+        int[] array = userInputReader.createArray(size);
+        MingMingRandom mingRandom = new MingMingRandom();
+        int[] result = mingRandom.handle(array);
+        Arrays.stream(result).forEach(System.out::println);
+    }
+}
